df0baab3fa56f4830813b1dd8beb06048112dac7
Abhij134 <148138113+Abhij134@users.noreply.github.com>
2026-04-14 15:33:09 +0530

    Fix Apple icon rendering by converting SVG polygons to universally supported paths

--- app/apple-icon.tsx
+++ app/apple-icon.tsx
@@ -23,14 +23,14 @@ export default function Icon() {
                 }}
             >
                 <svg viewBox="0 0 200 200" width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-                    <polygon points="40,165 70,135 10,165" fill="#4b5563" />
-                    <polygon points="40,105 70,75 70,135 40,165" fill="#10b981" />
+                    <path d="M 40 165 L 70 135 L 10 165 Z" fill="#4b5563" />
+                    <path d="M 40 105 L 70 75 L 70 135 L 40 165 Z" fill="#10b981" />
 
-                    <polygon points="85,165 115,135 55,165" fill="#4b5563" />
-                    <polygon points="85,60 115,30 115,135 85,165" fill="#10b981" />
+                    <path d="M 85 165 L 115 135 L 55 165 Z" fill="#4b5563" />
+                    <path d="M 85 60 L 115 30 L 115 135 L 85 165 Z" fill="#10b981" />
 
-                    <polygon points="130,165 160,135 100,165" fill="#4b5563" />
-                    <polygon points="130,80 160,50 160,135 130,165" fill="#10b981" />
+                    <path d="M 130 165 L 160 135 L 100 165 Z" fill="#4b5563" />
+                    <path d="M 130 80 L 160 50 L 160 135 L 130 165 Z" fill="#10b981" />
                 </svg>
             </div>
         ),
